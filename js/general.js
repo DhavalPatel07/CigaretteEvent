9f557e58a0f7b89bf41fb17466665a5040f2f022
Dhaval Patel <30649833+DhavalPatel07@users.noreply.github.com>
2023-09-01 11:36:35 +0530

    Add files via upload

--- js/general.js
+++ js/general.js
@@ -1,6 +1,19 @@
 var $=jQuery.noConflict();
 $(document).ready(function(){
 
+
+  
+    $('.burn').show();
+    $('.tobacco').animate({
+      width: "0cm"
+    }, 10000);
+    /*$('.burn').animate({
+      left: "0cm"
+    }, 10000, function() {
+      $(this).fadeOut();
+    });*/
+
+
 // Animation
     (function($) {
       $.fn.visible = function(partial) {var $t = $(this), $w = $(window), viewTop = $w.scrollTop(), viewBottom = viewTop + $w.height(), _top = $t.offset().top, _bottom = _top + $t.height(), compareTop = partial === true ? _bottom : _top, compareBottom = partial === true ? _top : _bottom; return ((compareBottom <= viewBottom) && (compareTop >= viewTop)); }; })(jQuery); var win = $(window); var allMods = $(".animation-effect"); allMods.each(function(i, el) {var el = $(el); if (el.visible(true)) {el.addClass("come-in"); }; }); win.scroll(function(event) {allMods.each(function(i, el) {var el = $(el); if (el.visible(true)) {setTimeout(function(){el.addClass("come-in"); }, 600); }; /*else {el.removeClass("come-in"); } */ });
@@ -18,7 +31,7 @@ var s = skrollr.init();
 
 /*Banner Video*/
 
-
+ 
 /* Form Video */
 window.addEventListener('load', videoScroll);
 window.addEventListener('scroll', videoScroll);
